566b39fcab39208671579fc3f1d9d0aca418eb0b
YongHyeon <pyh4616@naver.com>
2023-12-21 04:34:59 +0900

    변수 표현식 사용

--- src/main/java/practice/thymeleaf/web/BasicController.java
+++ src/main/java/practice/thymeleaf/web/BasicController.java
@@ -1,9 +1,14 @@
 package practice.thymeleaf.web;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("/basic")
@@ -19,5 +24,35 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "<b>data!<b>");
         return "text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("A", 1);
+        User userB = new User("B", 2);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        HashMap<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        return "variable";
+    }
+
+    @Data
+    static class User {
+        private String name;
+        private int age;
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
