1dc63de045c059ddefd665b1cd009630701223c5
YongHyeon <pyh4616@naver.com>
2023-12-25 14:19:19 +0900

    주석

--- src/main/java/practice/thymeleaf/web/BasicController.java
+++ src/main/java/practice/thymeleaf/web/BasicController.java
@@ -127,4 +127,10 @@ public class BasicController {
         addUsers(model);
         return "/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "sample");
+        return "comments";
+    }
 }
